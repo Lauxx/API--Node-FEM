c830efe11e8feaf2549b79d8b8f3d8dadb7d4200
Lauxx <lm.nichols@hotmail.com>
2016-03-05 14:25:41 -0700

    able to set up a basic server with express and ejs

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
-
+var express = require('express');
+var app = express();
 var jsonData = {count: 12, message: 'hey'};
+
+var port = process.env.PORT || 8000;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res){
+	res.render('index')
+});
+
+app.get('/data', function (req, res){
+	res.json(jsonData)
+});
+
+
+app.listen(port); 
+console.log("cash money on " + port);
+
